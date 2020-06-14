33f06b1493df1c9cb25c0011e06b038c888a35d3
terrace339 <allen.ackroyd1020@gmail.com>
2020-06-14 14:11:08 +0900

    add UI almost completed!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 //! ページリープ機能
+//! メトロノーム機能
+//! 表示項目にnumを追加
+//! データをgogengo!rootにしてみる
+//! ビジュアル
+
+
+//∆ メトロノーム停止機能
+//∆ 全部意味が表示される時のインターバル
 //∆ メトロノーム速度選択機能
-//∆ メトロノーム機能
 //∆ メトロノーム音声
 //∆ メトロノームα波とかに変える
 //∆ ショートカットキー追加
-//∆ ビジュアル
 //∆ csvファイル選択機能
 //∆ ラストデータのバグに対応するために修正
-//∆ 表示項目にnumを追加
-
+//∆ 次ページ、全ページのプレビュー機能
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -18,7 +23,7 @@ import Main from './components/main'
 // import Prime from './components/prime.js'
 
 import * as d3 from 'd3';
-import databook from './root.csv';
+import databook from './root.csv';//§
 
 d3.csv(databook, function(databook) { console.log(''); });
 var pagebook = []
@@ -48,8 +53,10 @@ d3.csv(databook).then(function(databook) {
 
 class App extends React.Component {
   constructor(props) {
-    var fixed_data = [[0,'zero'],[1,'ichi'],[2,'zero'],[3,'ichi'],[4,'zero'],[5,'ichi'],[6,'zero'],[7,'ichi'],[8,'zero'],[9,'ichi']];
-    var view_data =  [[0,'zero'],[1,'ichi'],[2,'zero'],[3,'ichi'],[4,'zero'],[5,'ichi'],[6,'zero'],[7,'ichi'],[8,'zero'],[9,'ichi']];
+    // var fixed_data = [[0,'zero'],[1,'ichi'],[2,'zero'],[3,'ichi'],[4,'zero'],[5,'ichi'],[6,'zero'],[7,'ichi'],[8,'zero'],[9,'ichi']];
+    // var view_data =  [[0,'zero'],[1,'ichi'],[2,'zero'],[3,'ichi'],[4,'zero'],[5,'ichi'],[6,'zero'],[7,'ichi'],[8,'zero'],[9,'ichi']];
+    var fixed_data = [];
+    var view_data =  [];
     var hoge_data = [[],[]];
     super(props);
     this.state = ({
@@ -60,8 +67,8 @@ class App extends React.Component {
       data: [],
       databook: databook,
       page: 0,
-      metRoop: true,
-      now: new Date(),
+      playing: false,
+      bpm: 70,
       // hoge_data: ['default','','','','','','','','',''],
     })
     // this.getData = this.getData.bind(this);
@@ -73,7 +80,7 @@ class App extends React.Component {
     console.log(this.state.hoge_data)
   }
   // ーーーーーーーーーーーーーーーーーーーー
-  countRow() {
+  countRow = () => {
     //
     if(this.state.row < 10){
       // activate_view_gradually
@@ -87,7 +94,7 @@ class App extends React.Component {
       // hidden
       for(var i = this.state.row;i <= 10; i++){
         if(i != 10){
-          mdfd_data.push([ this.state.fixed_data[i][0],'----------' ])
+          mdfd_data.push([ this.state.fixed_data[i][0],'--------------------' ])
         }
       }
 
@@ -121,11 +128,13 @@ class App extends React.Component {
     // this.setState({hoge_data: pagebook[page]})
     var hoge_data = []
     for(var i = 0; i < pagebook[page].length; i++){
+      //∆ var num = pagebook[page][i]['num']
       console.log(pagebook[page][i])
       var root = pagebook[page][i]['root']
       console.log(root);
       var meaning = pagebook[page][i]['meaning']
       hoge_data.push([root,meaning])
+      //∆ hoge_data.push([num,root,meaning])
     }
     this.setState({fixed_data: hoge_data})
   }
@@ -148,32 +157,48 @@ class App extends React.Component {
   // };
   //
   // setInterval( this.countRow, 1000);
-  metRoop(){
-    for(var i = 0; i < 20; i++){
-      this.interval = setInterval(this.tick, 1000);
-      this.countRow();
-      console.log(i);
+  metRoop = () => {
+    if(!this.state.playing){
+      this.timer = setInterval(this.countRow,(60 / this.state.bpm) * 1000);
+      this.setState({playing: true})
     }
   }
-  // metRoop() {
-  //   var count = 0;
-  //   var countup = function(){
-  //     console.log(count++);
-  //     setTimeout(() => {
-  //       this.countRow();
-  //     }, 2000);
+  doNothing = () => {
+
+  }
+
+  // startStop = () => {
+  //   if(this.state.playing) {
+  //     // Stop the timer
+  //     clearInterval(this.timer);
+  //     this.setState({
+  //       playing: false
+  //     });
+  //   } else {
+  //     // Start a timer with the current BPM
+  //     this.timer = setInterval(this.playClick, (60 / this.state.bpm) * 1000);
+  //     this.setState({
+  //       count: 0,
+  //       playing: true
+  //       // Play a click "immediately" (after setState finishes)
+  //     }, this.playClick);
   //   }
-  //   // const timer = setInterval(this.countRow(), 3000);
   // }
+
+
   render(){
     return (
       <div>
-        <button onClick={() => {this.countRow()}}>row+</button>
+
         {/*
+        <button onClick={() => {this.countRow()}}>row+</button>
         <button onClick={ () => {this.popCsv()}} >Hide!</button>
         */}
-        <button onClick={ () => {this.minusPage()}}>-</button>
-        <button onClick={ () => {this.plusPage()}}>+</button>
+        <button class='container' onClick={ () => {this.minusPage()}}>−</button>
+        <button class='container' onClick={ () => {this.plusPage()}}>+</button>
+        <button class='btn-circle-border-double' onClick={ () => {this.metRoop()}}>MetRoop</button>
+
+
 
         <table border='2' rules='all'>
           <tbody>
@@ -188,19 +213,22 @@ class App extends React.Component {
           </tbody>
         </table>
         <form onSubmit={() => {this.handleSubmit()}}>
-          <p>飛びたいページ<br></br>０から始まる</p>
-          {/* inputタグを追加してください */}
+
+          {/*
+            inputタグを追加してください
+            <p>飛びたいページ<br></br>０から始まる</p>
+          */}
           <input
             value={this.state.page}
             onChange={(event) => {this.handleEmailChange(event)}}
           />
           {/* 送信ボタンを追加してください */}
         </form>
-        <button class='setPage' onClick={ () => {this.setPage()}}>setPage</button>
-        <button class='setPage' onClick={ () => {this.metRoop()}}>metRoop</button>
-        <h3>{'now:' + (this.state.row - 1)}</h3>
-        <h3>{'next:' + this.state.row}</h3>
-        <h3>{'nowPage:' + this.state.page}</h3>
+        <button class='setPage container' onClick={ () => {this.setPage()}}>setPage</button>
+
+        <h3>{'Now:' + (this.state.row - 1)}</h3>
+        <h3>{'Next:' + this.state.row}</h3>
+        <h3>{'Page:' + this.state.page}</h3>
 
         <h3>{this.state.hoge_data}</h3>
       </div>
